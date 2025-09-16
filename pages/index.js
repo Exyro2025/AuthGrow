9d67ad65b0a5d1a56d644318f90221f88153e491
Exyro2025 <RLLEmail@proton.me>
2025-09-16 19:18:38 -0400

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{padding: 24, fontFamily: 'system-ui, sans-serif'}}>
+      <h1>AuthGrow</h1>
+      <p>If you can see this, the deployment worked 🎉</p>
+    </main>
+  );
+}
